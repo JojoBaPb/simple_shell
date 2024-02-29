1bb46eef6419b2fffc3d49b2a743085222e78caa
muhammedbalogun <muhammed.balogun123@gmail.com>
2024-02-29 05:10:50 +0000

    updated utils.c

--- utils.c
+++ utils.c
@@ -1,9 +1,16 @@
 #include "shell.h"
 
-/* Remove newline character from a string */
+/**
+ * remove_newline - Remove newline character from a string
+ * @str: string variable
+ *
+ * return: None
+ */
+
 void remove_newline(char *str)
 {
 	int length = strlen(str);
+
 	if (length > 0 && str[length - 1] == '\n')
 	{
 		str[length - 1] = '\0';
